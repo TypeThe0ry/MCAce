aa6b19bd9cd65bc11bf2eecde172ab15bcac875e
NLCS-Yutian-Fu <104051227+TypeThe0ry@users.noreply.github.com>
2026-08-24 15:03:04 +0800

    fix: import probe socket address type

--- mcace-runtime-integration/src/test/java/com/ellan/mcace/runtime/MinecraftProxyPlayerProbeTest.java
+++ mcace-runtime-integration/src/test/java/com/ellan/mcace/runtime/MinecraftProxyPlayerProbeTest.java
@@ -30,6 +30,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
